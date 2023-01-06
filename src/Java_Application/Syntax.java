2e3df7248d399fd8e671972c87f80bc4c0f5f2ac
Furkan Bülbül <74109076+FurkanBulbul@users.noreply.github.com>
2023-01-06 12:28:00 +0300

    Java Foundation Applications added

---
+++ src/Java_Application/Syntax.java
@@ -0,0 +1,10 @@
+package Java_Application;
+
+public class Syntax {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/Java_Application/Variables.java
@@ -0,0 +1,9 @@
+package Java_Application;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+       String dogBreed="poodle";
+    }
+}
