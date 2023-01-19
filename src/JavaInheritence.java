15cf28f408a0e71c505be6fd7e163528ac5923a3
Furkan Bülbül <74109076+FurkanBulbul@users.noreply.github.com>
2023-01-19 14:42:13 +0300

    JavaInheritence.java added

---
+++ src/JavaInheritence.java
@@ -0,0 +1,31 @@
+package Java_Application;
+
+public class JavaInheritence {
+
+    public static void main(String[] args) {
+        Car carObject = new Car();
+        carObject.rev();
+        System.out.println(carObject.getMake()+" " + carObject.getModel());
+    }
+}
+
+class Vehicle {
+    private String make = "BMW";
+
+    public String getMake() {
+        return make;
+    }
+
+    public void rev() {
+        System.out.println("Vrooom!!");
+    }
+}
+
+class Car extends Vehicle {
+    private String model = "3 series";
+
+    public String getModel() {
+        return model;
+    }
+
+}
