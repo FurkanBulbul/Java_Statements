3745b3a1d0cd189e83f22e42b551eb3dc3facbb6
Furkan Bülbül <74109076+FurkanBulbul@users.noreply.github.com>
2023-01-30 23:38:21 +0300

    JavaList.java added

---
+++ src/JavaList.java
@@ -0,0 +1,20 @@
+package Java_Application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaList {
+
+    public static void main(String[] args) {
+
+        List<String> animals = new ArrayList<String>();
+
+        animals.add("cat");
+        animals.add("dog");
+
+        for (int i=0; i<animals.size(); i++){
+            System.out.println(animals.get(i));
+        }
+
+    }
+}
